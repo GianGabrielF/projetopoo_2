6733fbc2431861dea7577baa3089366a9b45a3f9
GianGabrielF <gian.s12@aluno.ifsc.edu.br>
2023-03-23 13:43:16 -0300

    projeto novo

---
+++ src/Main/java/engtelecom/poo/App.java
@@ -0,0 +1,17 @@
+package engtelecom.poo;
+
+public class App {
+
+    public static void olaMundo(String mensagem){
+        System.out.println("olá mundo");
+
+    }
+
+    public void boaTarde(){
+        System.out.println("Boa Tarde");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
